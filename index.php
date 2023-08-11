333e00ada1243bda3f2f72936324ff4a64cdb2fb
emeraru <emerarural@gmail.com>
2023-08-11 21:41:08 +0900

    remo:master: index.php, oinarisan1.pngを登録 🐈

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang = 'ja'>
+<head>
+	<meta charset = 'utf-8'>
+	<meta name = 'description' content = 'githubを利用する 🐈'>
+	<meta name = 'viewport' content = 'width = device-width, initial-scale = 1, minimum-scale = 1'>
+
+	<link rel = 'stylesheet' href = './style.css'>
+	<link rel = 'icon' href = './oinarisan1.png'>
+	<link rel = 'apple-touch-icon-precomposed' href = './oinarisan1.png'>
+
+	<title>github 🐈</title>
+</head>
+<body>
+	<h1>githubを利用する 🐈</h1>
+	<p>データの上げ下げができるようにする 🐄</p>
+</body>
+</html>
